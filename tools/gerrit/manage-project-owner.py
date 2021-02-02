47759d91a3be76b5b3e2b0b672263d8f1b4373f9
Sunjoo Park <all4dich@gmail.com>
2021-02-02 16:08:28 +0900

    manage-project-owner.py: Show result at the end

--- tools/gerrit/manage-project-owner.py
+++ tools/gerrit/manage-project-owner.py
@@ -5,12 +5,12 @@ import requests
 import json
 
 arg_parser = argparse.ArgumentParser()
-arg_parser.add_argument("--username", required=True)
-arg_parser.add_argument("--password", required=True)
-arg_parser.add_argument("--url", required=True)
-arg_parser.add_argument("--project", required=True)
-arg_parser.add_argument("--target-user", required=True)
-arg_parser.add_argument("--remove-user", action="store_true")
+arg_parser.add_argument("--username", required=True, help="Gerrit Administrator Username")
+arg_parser.add_argument("--password", required=True, help="Gerrit Administrator Password")
+arg_parser.add_argument("--url", required=True, help="Gerrit Web Url")
+arg_parser.add_argument("--project", required=True, help="Project name on Gerrit")
+arg_parser.add_argument("--target-user", required=True, help="Username to be added on Owner")
+arg_parser.add_argument("--remove-user", action="store_true", help="Remove a specified user from Owner")
 args = arg_parser.parse_args()
 
 gerrit_api_root = f"{args.url}a/"
@@ -19,8 +19,16 @@ gerrit_password = args.password
 target_user = args.target_user
 
 
-def get_owner_info(access_info_str):
-    access_info = json.loads(access_info_str)
+def get_project_info(project_name):
+    project_name_uri = parse.quote(project_name, safe="")
+    get_project_url = f"{gerrit_api_root}projects/{project_name_uri}/access"
+    r = requests.get(get_project_url, auth=(gerrit_username, gerrit_password))
+    r_obj = json.loads(r.text.replace(")]}'", ""))
+    return r_obj
+
+
+def get_owner_info(project_name):
+    access_info = get_project_info(project_name)
     group_info = access_info['groups']
     owner_rule = access_info['local']['refs/*']['permissions']['owner']['rules']
     owner_id = next(iter(owner_rule))
@@ -28,29 +36,28 @@ def get_owner_info(access_info_str):
     return {"id": owner_id, "name": owner_group['name'], "url": owner_group['url']}
 
 
-def add_user_to_owner_group(access_info_str):
-    owner_info = get_owner_info(access_info_str)
+def add_user_to_owner_group(project_name, new_user):
+    owner_info = get_owner_info(project_name)
     owner_id = owner_info["id"]
-    add_member_to_group = f"{gerrit_api_root}groups/{owner_id}/members/{target_user}"
+    add_member_to_group = f"{gerrit_api_root}groups/{owner_id}/members/{new_user}"
     add_member_req = requests.put(add_member_to_group, auth=(gerrit_username, gerrit_password))
     return add_member_req.status_code
 
 
-def remove_user_from_owner_group(access_info_str):
-    owner_info = get_owner_info(access_info_str)
+def remove_user_from_owner_group(project_name, removed_user):
+    owner_info = get_owner_info(project_name)
     owner_id = owner_info["id"]
-    remove_group_member = f"{gerrit_api_root}groups/{owner_id}/members/{target_user}"
+    remove_group_member = f"{gerrit_api_root}groups/{owner_id}/members/{removed_user}"
     remove_member_req = requests.delete(remove_group_member, auth=(gerrit_username, gerrit_password))
     return remove_member_req.status_code
 
 
 # Main Area #
-project_name = parse.quote(args.project, safe="")
-get_project_url = f"{gerrit_api_root}projects/{project_name}/access"
-r = requests.get(get_project_url, auth=(gerrit_username, gerrit_password))
-if r.status_code == 200:
-    print(r.text)
-    if args.remove_user:
-        remove_user_from_owner_group(r.text.replace(")]}'", ""))
-    else:
-        add_user_to_owner_group(r.text.replace(")]}'", ""))
+return_code = ""
+job_type = {True: 'Remove user', False: 'Add user'}
+if args.remove_user:
+    return_code = remove_user_from_owner_group(args.project, target_user)
+else:
+    return_code = add_user_to_owner_group(args.project, target_user)
+print(f"{__file__}: Job type = {job_type[args.remove_user]}")
+print(f"{__file__}: Result = {return_code}")
