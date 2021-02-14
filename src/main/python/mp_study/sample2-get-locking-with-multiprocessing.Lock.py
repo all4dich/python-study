61212dbb201c8407bff1933c958c01bd1d283f2d
Sunjoo Park <all4dich@gmail.com>
2021-02-14 17:44:25 +0900

    sample2: Update the lock mechanism sample

--- src/main/python/mp_study/sample2-get-locking-with-multiprocessing.Lock.py
+++ src/main/python/mp_study/sample2-get-locking-with-multiprocessing.Lock.py
@@ -9,33 +9,37 @@ import sys
 #   : https://docs.python.org/3/library/functools.html#functools.partial
 
 
-def use_manage(first, lock):
+def use_manage(first):
     print(current_process())
     while True:
-        lock.acquire()
         try:
+            lock.acquire()
             v = first.pop()
             print(v)
         except IndexError:
             # print("Exception1")
             # print(sys.exc_info()[0])
-            lock.release()
             print("DEBUG: Lock released ")
             return
         finally:
             lock.release()
 
 
+def init_lock(l):
+    global lock
+    lock = l
+
+
 if __name__ == "__main__":
     manage = Manager()
     list1 = manage.list()
     print("Hello World")
-    pool = Pool(4)
-    a = list(range(100))
+    a = list(range(10))
     for i in a:
         list1.append(i)
-    lock = manage.Lock()
+    l = Lock()
+    pool = Pool(4, initializer=init_lock, initargs=(l,))
     for i in range(4):
-        pool.apply_async(use_manage, args=(list1, lock,))
+        pool.apply_async(use_manage, args=(list1,))
     pool.close()
     pool.join()
